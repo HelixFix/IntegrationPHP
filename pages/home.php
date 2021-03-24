18a1a85618210219f9056c0cf04bf92cb71c8f12
HelixFix <jerome.rinner@gmail.com>
2021-03-24 11:05:20 +0100

    :construction: Add search and radio

---
+++ pages/home.php
@@ -0,0 +1,29 @@
+<div class="container">
+
+    <form method="post" action="">
+
+        <!-- Default radio -->
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
+            <label class="form-check-label" for="flexRadioDefault1"> Froid </label>
+        </div>
+
+        <!-- Default checked radio -->
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked />
+            <label class="form-check-label" for="flexRadioDefault2"> Chaud </label>
+        </div>
+        <br>
+        <div class="input-group">
+            <div class="form-outline">
+                <input type="search" id="form1" class="form-control" />
+                <label class="form-label" for="form1">Recherche</label>
+            </div>
+            <button type="submit" name="subrche" class="btn btn-primary">
+                <i class="fas fa-search"></i>
+            </button>
+        </div>
+
+    </form>
+
+</div>
