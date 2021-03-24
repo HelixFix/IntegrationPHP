3fd30c839959b10ed7f6c0c10e8217f1702f9345
HelixFix <jerome.rinner@gmail.com>
2021-03-24 11:16:52 +0100

    :construction: Add checkox in contact form

--- pages/contact.php
+++ pages/contact.php
@@ -11,6 +11,24 @@
                 <label class="form-label" for="form4Example2">Email address</label>
             </div>
 
+            <!-- Default checkbox -->
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                <label class="form-check-label" for="flexCheckDefault">
+                    Sujet 1
+                </label>
+            </div>
+
+            <!-- Checked checkbox -->
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked />
+                <label class="form-check-label" for="flexCheckChecked">
+                    Sujet 2
+                </label>
+            </div>
+
+            <br>
+
             <!-- Message input -->
             <div class="form-outline mb-4">
                 <textarea name="contmail" class="form-control" id="form4Example3" rows="4"></textarea>
@@ -20,7 +38,7 @@
 
             <!-- Submit button -->
             <button type="submit" class="btn btn-primary btn-block mb-4" value="Envoyer">Send</button>
-            
+
             <!-- anti bot -->
             <input type="text" name="pays" style="border: none;width:20px;">
 
@@ -38,9 +56,9 @@
         $mailheaders .= "X-Mailer: PHP\n";
         $mailheaders .= "X-abuse-contact: abuse@1and1.fr\n";
         $mailheaders .= "X-auth-smtp-user: pierre@iziasys.com\n";
-        
-        if($_POST['pays'] == '') mail($_POST['maildest'], 'Un e-mail de votre site Internet', $_POST['contmail'],$mailheaders);
-        ?>
+
+        if ($_POST['pays'] == '') mail($_POST['maildest'], 'Un e-mail de votre site Internet', $_POST['contmail'], $mailheaders);
+    ?>
 
         <!-- Sinon on dit : -->
         <b>Merci de nous avoir contacter votre message sera traité rapidement.</b>
