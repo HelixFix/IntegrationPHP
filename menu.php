8f998ca5f45c308ec004ea55aca22036e74d272b
HelixFix <jerome.rinner@gmail.com>
2021-04-24 10:54:59 +0200

    :lipstick: New navbar

--- menu.php
+++ menu.php
@@ -18,25 +18,58 @@
                 <!--ms-auto placement de l'élément à droite-->
                 <ul class="navbar-nav ">
 
-                    <li class="nav-item">
-                        <a href="index.php?page=home" class="nav-link">Accueil</a>
-                    </li>
 
                     <li class="nav-item">
-                        <a href="index.php?page=produits" class="nav-link">Produits</a>
+
+                        <a href="index.php?page=home">
+                            <div class="icon">
+                                <i class="fa fa-home" aria-hidden="true"></i>
+                                <i class="fa fa-home" aria-hidden="true"></i>
+                            </div>
+                            <div class="name"><span data-text="Accueil">Accueil</span></div>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+
+                        <a href="index.php?">
+                            <div class="icon">
+                                <i class="fa fa-utensils" aria-hidden="true"></i>
+                                <i class="fa fa-utensils" aria-hidden="true"></i>
+                            </div>
+                            <div class="name"><span data-text="Produits">Produits</span></div>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+
+                        <a href="index.php?page=societe">
+                            <div class="icon">
+                                <i class="fa fa-building" aria-hidden="true"></i>
+                                <i class="fa fa-building" aria-hidden="true"></i>
+                            </div>
+                            <div class="name"><span data-text="Société">Société</span></div>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+
+                        <a href="index.php?page=contact">
+                            <div class="icon">
+                                <i class="fa fa-address-card" aria-hidden="true"></i>
+                                <i class="fa fa-address-card" aria-hidden="true"></i>
+                            </div>
+                            <div class="name"><span data-text="Contact">Contact</span></div>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+
+                        <a href="index.php?page=produits">
+                            <div class="icon">
+                                <i class="fa fa-video" aria-hidden="true"></i>
+                                <i class="fa fa-video" aria-hidden="true"></i>
+                            </div>
+                            <div class="name"><span data-text="Gestion">Gestion</span></div>
+                        </a>
                     </li>
 
-                    <li class="nav-item">
-                        <a href="index.php?page=societe" class="nav-link">Société</a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a href="index.php?page=contact" class="nav-link">Contact</a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a href="index.php?page=gestion" class="nav-link">Gestion</a>
-                    </li>
 
                 </ul>
             </div>
