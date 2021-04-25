362432391dd521488f2b7a7d2d8564a8173f8f98
HelixFix <jerome.rinner@gmail.com>
2021-04-25 19:47:05 +0200

    Add content in société page

--- pages/societe.php
+++ pages/societe.php
@@ -1,145 +1,189 @@
-<div class="experience-component experience-assets-brandcontent-brandContentHeader">
+<div class="container">
 
-<div class="pi-BrandContentHeader pi-BrandContentHeader--about">
-    <div class="pi-BrandContentHeader-visual">
-        <picture>
-            
-                <source media="(max-width: 767px)" srcset="https://www.picard.fr/on/demandware.static/-/Sites-picard-Library/default/dw7efc21fe/brandcontent/apropos/banner-mobile.jpg">
-            
-            
-                <img class="pi-BrandContentHeader-visualImg" src="https://www.picard.fr/on/demandware.static/-/Sites-picard-Library/default/dwdd8ea77e/brandcontent/apropos/banner.jpg" alt="">
-            
-        </picture>
-    </div>
-    <div class="pi-BrandContentHeader-content">
-        
-            <h1 class="pi-BrandContentHeader-contentTitle ">
-                 À propos de be fruit
-            </h1>
-        
-        
-            <p class="pi-BrandContentHeader-contentDescription">
-                Bienvenue aux amoureux du bon.
-            </p>
-        
-    </div>
-</div>
-</div><div class="experience-component experience-layouts-div1column"><div class="pi-BrandContent-row pi-BrandContent-row--noMarginTop is-lgCenter"><div class="experience-component experience-assets-brandcontent-quote"><div class="pi-Quote-container">
-    <h2 class="pi-Quote-title u-Hidden--lg">
-         
-    </h2>
-    <blockquote class="pi-BrandContent-quote pi-Quote pi-Quote--withImage">
-        <h2 class="pi-Quote-title u-Visible--lg">
-             
-        </h2>
-        <p class="pi-Quote-text">
-            <span class="pi-Quote-img pi-Quote-img--start"></span>
-            Nous souhaitons la bienvenue à tous ceux qui aiment manger, cuisiner, partager, découvrir, célèbrer, vibrer.....Depuis près de 50 ans,nous n'avons de cesse avec nos collaborateurs et nos producteurs partenaires, de faire grandir chaque jour un peu plus l'amour du bon.
-            <span class="pi-Quote-img pi-Quote-img--end"></span>
-        </p>
-        
-            <footer>
-                <p class="pi-Quote-author">Cathy Collart-Geiger, Présidente Directrice Générale de be fruit</p>
-            </footer>
-        
-    </blockquote>
-</div>
-</div></div>
+	<div class="experience-component experience-assets-brandcontent-brandContentHeader">
 
-</div><div class="experience-component experience-layouts-div1column"><div class="pi-BrandContent-row is-full"><div class="experience-component experience-assets-brandcontent-pushCorpo"><div class="pi-PushCorpo">
-    <div class="pi-PushCorpo-visual">
-        <picture>
-            <source media="(max-width: 767px)" srcset="https://www.picard.fr/on/demandware.static/-/Sites-picard-Library/default/dwcc80b417/brandcontent/apropos/corpo-1-mobile.jpg">
-            <img class="pi-PushCorpo-visualImg" src="https://www.picard.fr/on/demandware.static/-/Sites-picard-Library/default/dwb1baf7d7/brandcontent/apropos/corpo-1.jpg" alt="">
-        </picture>
-    </div>
-    <div class="pi-PushCorpo-content ">
-        <h2 class="pi-PushCorpo-title">Depuis plus d’un siècle</h2>
-        <p class="pi-PushCorpo-description is-alwaysSmall">Initialement connu sous le nom des «Glacières de Fontainebleau» en 1906, Raymond be fruit crée ce qui est déjà et ce qui deviendra be fruit : le pionnier du secteur de la surgélation !</p>
+		<div class="pi-BrandContentHeader pi-BrandContentHeader--about">
+			<div class="pi-BrandContentHeader-visual">
+				<picture>
 
-    </div>
-</div></div></div>
-
-</div><div class="experience-component experience-layouts-div1column"><div class="pi-BrandContent-row is-lgRight"><div class="pi-PushRichContent pi-PushRichContent--about"><div class="pi-PushRichContent-col">
-    <h2 class="pi-PushRichContent-title">Pour proposer les meilleurs produits</h2>
-    
-        <img class="pi-PushRichContent-visualImg pi-PushRichContent-visualImg--mobile" src="https://www.picard.fr/on/demandware.static/-/Sites-picard-Library/default/dw9d72ba5f/brandcontent/apropos/best-products-mobile.jpg" alt="">
-    
-    <div class="pi-PushRichContent-text">
-        <p>
-            Qui n’a jamais goûté le moelleux au chocolat be fruit ? Vous êtes plutôt salé ? Vous connaissez
-                    la pizza 4 saisons bio dans ce cas.
-        </p>
-    </div>
-    
-
-    
-
-    
-</div>
-<div class="pi-PushRichContent-col">
-    
-        <div class="pi-PushRichContent-visual">
-            <img class="pi-PushRichContent-visualImg" src="https://www.picard.fr/on/demandware.static/-/Sites-picard-Library/default/dweb06ec27/brandcontent/apropos/best-products.jpg" alt="">
-        </div>
-       
-</div>
-</div></div>
-
-</div><div class="experience-component experience-layouts-div1column"><div class="pi-BrandContent-row"><div class="experience-component experience-assets-brandcontent-pushCorpo"><div class="pi-PushCorpo">
-    <div class="pi-PushCorpo-visual">
-        <picture>
-            <source media="(max-width: 767px)" srcset="https://www.picard.fr/on/demandware.static/-/Sites-picard-Library/default/dwaf2cbf9f/brandcontent/apropos/corpo-2-mobile.jpg">
-            <img class="pi-PushCorpo-visualImg" src="https://www.picard.fr/on/demandware.static/-/Sites-picard-Library/default/dwbf9ee967/brandcontent/apropos/corpo-2.jpg" alt="">
-        </picture>
-    </div>
-    <div class="pi-PushCorpo-content pi-PushCorpo-content--end">
-        <h2 class="pi-PushCorpo-title">Avec des équipes engagées </h2>
-        <p class="pi-PushCorpo-description ">be fruit s’attache à proposer à ses clients un accueil et des conseils pour leur vie alimentaire de tous les jours. Voilà comment les équipes be fruit conçoivent leur métier depuis plus de trente ans.</p>
-        
-    </div>
-</div></div><div class="experience-component experience-layouts-div1column"><div class="pi-BrandContent-row is-lgCenter"><div class="experience-component experience-assets-brandcontent-3keyNumbers"> <div class="pi-KeyNumbers">
-    <div class="pi-KeyNumbers-content">
-        <h2 class="pi-KeyNumbers-title is-xlRight" style="color:#2D4999;">Car le plaisir du goût <br> compte pour nous</h2>
-        <ul class="pi-KeyNumbers-list pi-KeyNumbers-list--lg3Cols"><li style="border-color: #F47935;" class="pi-KeyNumbers-item"><p class="pi-KeyNumbers-num" style="color: #F47935;">1025</p>
-<p class="pi-KeyNumbers-text">
-    Le nombre de nos magasins partout en France
-</p>
-</li><li style="border-color: #F47935;" class="pi-KeyNumbers-item"><p class="pi-KeyNumbers-num" style="color: #F47935;">30</p>
-<p class="pi-KeyNumbers-text">
-    Le nombre de nouveaux magasins par an
-</p>
-</li><li style="border-color: #F47935;" class="pi-KeyNumbers-item"><p class="pi-KeyNumbers-num" style="color: #F47935;">950</p>
-<p class="pi-KeyNumbers-text">
-    Le nombre de recrutements par an en CDI
-</p>
-</li></ul>
-    </div>
+					<source media="(max-width: 767px)" srcset="https://www.picard.fr/on/demandware.static/-/Sites-picard-Library/default/dw7efc21fe/brandcontent/apropos/banner-mobile.jpg">
 
 
-    </div>
-</div>
-</div></div>
+					<img class="pi-BrandContentHeader-visualImg" src="https://www.picard.fr/on/demandware.static/-/Sites-picard-Library/default/dwdd8ea77e/brandcontent/apropos/banner.jpg" alt="">
 
-</div></div>
+				</picture>
+			</div>
+			<div class="pi-BrandContentHeader-content">
 
-</div><div class="experience-component experience-layouts-div1column"><div class="pi-BrandContent-row"><div class="experience-component experience-assets-brandcontent-pushCorpo"><div class="pi-PushCorpo">
-    <div class="pi-PushCorpo-visual">
-        <picture>
-            <source media="(max-width: 767px)" srcset="https://www.picard.fr/on/demandware.static/-/Sites-picard-Library/default/dw0aef144e/brandcontent/apropos/RSEmobile.png">
-            <img class="pi-PushCorpo-visualImg" src="https://www.picard.fr/on/demandware.static/-/Sites-picard-Library/default/dw82809758/brandcontent/apropos/RSE.png" alt="">
-        </picture>
-    </div>
-    <div class="pi-PushCorpo-content pi-PushCorpo-content--end">
-        <h2 class="pi-PushCorpo-title">be fruit, une entreprise responsable</h2>
-        <p class="pi-PushCorpo-description is-alwaysSmall">Chez be fruit, nous nous engageons depuis plus de 10 ans en faveur du développement durable. La RSE au sein de notre enseigne ne se résume pas à 3 lettres, mais plutôt à 3 piliers qui nous animent et guident nos actions au quotidien : l’environnement, le social et le sociétal. 
+				<h1 class="pi-BrandContentHeader-contentTitle ">
+					À propos de be fruit
+				</h1>
 
-Nous nous mobilisons pour réduire notre empreinte environnementale, pour garantir à nos collaborateurs un quotidien professionnel de qualité, et pour développer les bassins territoriaux dans lesquels nous sommes implantés.  
 
-Dans une approche apprenante, d’humilité et d’amélioration continue, nous œuvrons pour que chaque année soit meilleure que la précédente sur l’ensemble de ces sujets et ainsi faire de be fruit une marque toujours plus durable, plus humaine et plus citoyenne.
-</p>
-   
-    </div>
-</div></div></div>
+				<p class="pi-BrandContentHeader-contentDescription">
+					Bienvenue aux amoureux du bon.
+				</p>
 
-</div>
+			</div>
+		</div>
+	</div>
+	<div class="experience-component experience-layouts-div1column">
+		<div class="pi-BrandContent-row pi-BrandContent-row--noMarginTop is-lgCenter">
+			<div class="experience-component experience-assets-brandcontent-quote">
+				<div class="pi-Quote-container">
+					<h2 class="pi-Quote-title u-Hidden--lg">
+
+					</h2>
+					<blockquote class="pi-BrandContent-quote pi-Quote pi-Quote--withImage">
+						<h2 class="pi-Quote-title u-Visible--lg">
+
+						</h2>
+						<p class="pi-Quote-text">
+							<span class="pi-Quote-img pi-Quote-img--start"></span>
+							Nous souhaitons la bienvenue à tous ceux qui aiment manger, cuisiner, partager, découvrir, célèbrer, vibrer.....Depuis près de 50 ans,nous n'avons de cesse avec nos collaborateurs et nos producteurs partenaires, de faire grandir chaque jour un peu plus l'amour du bon.
+							<span class="pi-Quote-img pi-Quote-img--end"></span>
+						</p>
+
+						<footer>
+							<p class="pi-Quote-author">Cathy Collart-Geiger, Présidente Directrice Générale de be fruit</p>
+						</footer>
+
+					</blockquote>
+				</div>
+			</div>
+		</div>
+
+	</div>
+	<div class="experience-component experience-layouts-div1column">
+		<div class="pi-BrandContent-row is-full">
+			<div class="experience-component experience-assets-brandcontent-pushCorpo">
+				<div class="pi-PushCorpo">
+					<div class="pi-PushCorpo-visual">
+						<picture>
+							<source media="(max-width: 767px)" srcset="https://www.picard.fr/on/demandware.static/-/Sites-picard-Library/default/dwcc80b417/brandcontent/apropos/corpo-1-mobile.jpg">
+							<img class="pi-PushCorpo-visualImg" src="https://www.picard.fr/on/demandware.static/-/Sites-picard-Library/default/dwb1baf7d7/brandcontent/apropos/corpo-1.jpg" alt="">
+						</picture>
+					</div>
+					<div class="pi-PushCorpo-content ">
+						<h2 class="pi-PushCorpo-title">Depuis plus d’un siècle</h2>
+						<p class="pi-PushCorpo-description is-alwaysSmall">Initialement connu sous le nom des «Glacières de Fontainebleau» en 1906, Raymond be fruit crée ce qui est déjà et ce qui deviendra be fruit : le pionnier du secteur de la surgélation !</p>
+
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
+	<div class="experience-component experience-layouts-div1column">
+		<div class="pi-BrandContent-row is-lgRight">
+			<div class="pi-PushRichContent pi-PushRichContent--about">
+				<div class="pi-PushRichContent-col">
+					<h2 class="pi-PushRichContent-title">Pour proposer les meilleurs produits</h2>
+
+					<img class="pi-PushRichContent-visualImg pi-PushRichContent-visualImg--mobile" src="https://www.picard.fr/on/demandware.static/-/Sites-picard-Library/default/dw9d72ba5f/brandcontent/apropos/best-products-mobile.jpg" alt="">
+
+					<div class="pi-PushRichContent-text">
+						<p>
+							Qui n’a jamais goûté le moelleux au chocolat be fruit ? Vous êtes plutôt salé ? Vous connaissez
+							la pizza 4 saisons bio dans ce cas.
+						</p>
+					</div>
+
+
+
+
+
+				</div>
+				<div class="pi-PushRichContent-col">
+
+					<div class="pi-PushRichContent-visual">
+						<img class="pi-PushRichContent-visualImg" src="https://www.picard.fr/on/demandware.static/-/Sites-picard-Library/default/dweb06ec27/brandcontent/apropos/best-products.jpg" alt="">
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+	</div>
+	<div class="experience-component experience-layouts-div1column">
+		<div class="pi-BrandContent-row">
+			<div class="experience-component experience-assets-brandcontent-pushCorpo">
+				<div class="pi-PushCorpo">
+					<div class="pi-PushCorpo-visual">
+						<picture>
+							<source media="(max-width: 767px)" srcset="https://www.picard.fr/on/demandware.static/-/Sites-picard-Library/default/dwaf2cbf9f/brandcontent/apropos/corpo-2-mobile.jpg">
+							<img class="pi-PushCorpo-visualImg" src="https://www.picard.fr/on/demandware.static/-/Sites-picard-Library/default/dwbf9ee967/brandcontent/apropos/corpo-2.jpg" alt="">
+						</picture>
+					</div>
+					<div class="pi-PushCorpo-content pi-PushCorpo-content--end">
+						<h2 class="pi-PushCorpo-title">Avec des équipes engagées </h2>
+						<p class="pi-PushCorpo-description ">be fruit s’attache à proposer à ses clients un accueil et des conseils pour leur vie alimentaire de tous les jours. Voilà comment les équipes be fruit conçoivent leur métier depuis plus de trente ans.</p>
+
+					</div>
+				</div>
+			</div>
+			<div class="experience-component experience-layouts-div1column">
+				<div class="pi-BrandContent-row is-lgCenter">
+					<div class="experience-component experience-assets-brandcontent-3keyNumbers">
+						<div class="pi-KeyNumbers">
+							<div class="pi-KeyNumbers-content">
+								<h2 class="pi-KeyNumbers-title is-xlRight" style="color:#2D4999;">Car le plaisir du goût <br> compte pour nous</h2>
+								<ul class="pi-KeyNumbers-list pi-KeyNumbers-list--lg3Cols">
+									<li style="border-color: #F47935;" class="pi-KeyNumbers-item">
+										<p class="pi-KeyNumbers-num" style="color: #F47935;">1025</p>
+										<p class="pi-KeyNumbers-text">
+											Le nombre de nos magasins partout en France
+										</p>
+									</li>
+									<li style="border-color: #F47935;" class="pi-KeyNumbers-item">
+										<p class="pi-KeyNumbers-num" style="color: #F47935;">30</p>
+										<p class="pi-KeyNumbers-text">
+											Le nombre de nouveaux magasins par an
+										</p>
+									</li>
+									<li style="border-color: #F47935;" class="pi-KeyNumbers-item">
+										<p class="pi-KeyNumbers-num" style="color: #F47935;">950</p>
+										<p class="pi-KeyNumbers-text">
+											Le nombre de recrutements par an en CDI
+										</p>
+									</li>
+								</ul>
+							</div>
+
+
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+
+	<div class="experience-component experience-layouts-div1column">
+		<div class="pi-BrandContent-row">
+			<div class="experience-component experience-assets-brandcontent-pushCorpo">
+				<div class="pi-PushCorpo">
+					<div class="pi-PushCorpo-visual">
+						<picture>
+							<source media="(max-width: 767px)" srcset="https://www.picard.fr/on/demandware.static/-/Sites-picard-Library/default/dw0aef144e/brandcontent/apropos/RSEmobile.png">
+							<img class="pi-PushCorpo-visualImg" src="https://www.picard.fr/on/demandware.static/-/Sites-picard-Library/default/dw82809758/brandcontent/apropos/RSE.png" alt="">
+						</picture>
+					</div>
+					<div class="pi-PushCorpo-content pi-PushCorpo-content--end">
+						<h2 class="pi-PushCorpo-title">be fruit, une entreprise responsable</h2>
+						<p class="pi-PushCorpo-description is-alwaysSmall">Chez be fruit, nous nous engageons depuis plus de 10 ans en faveur du développement durable. La RSE au sein de notre enseigne ne se résume pas à 3 lettres, mais plutôt à 3 piliers qui nous animent et guident nos actions au quotidien : l’environnement, le social et le sociétal.
+
+							Nous nous mobilisons pour réduire notre empreinte environnementale, pour garantir à nos collaborateurs un quotidien professionnel de qualité, et pour développer les bassins territoriaux dans lesquels nous sommes implantés.
+
+							Dans une approche apprenante, d’humilité et d’amélioration continue, nous œuvrons pour que chaque année soit meilleure que la précédente sur l’ensemble de ces sujets et ainsi faire de be fruit une marque toujours plus durable, plus humaine et plus citoyenne.
+						</p>
+
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+
